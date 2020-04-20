ab80bb9e001bce5c1a42997f7ef76a93ec4d7bb7
Nam Nguyen <namnd86@gmail.com>
2020-04-20 21:52:18 +1000

    snapshot

---
+++ test/App.spec.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { shallow } from "enzyme"
+import { shallowToJson } from "enzyme-to-json"
+import App from "../src/App"
+
+describe('App', () => {
+    it('should render correctly', () => {
+        const output = shallow(<App />)
+
+        expect(shallowToJson(output)).toMatchSnapshot()
+    });
+});
