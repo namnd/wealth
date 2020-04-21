32c006b72b9caeecb4d25ab5204f6c00a15d1a24
Nam Nguyen <namnd86@gmail.com>
2020-04-21 22:07:04 +1000

    file extensions rule and settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,23 @@ module.exports = {
     ],
     rules: {
         'indent': ['error', 4],
-        "react/jsx-filename-extension": [0],
+        'react/jsx-filename-extension': [0],
+        'import/extensions': ['error', 'ignorePackages', {
+            'js': 'never',
+            'jsx': 'never',
+            'ts': 'never',
+            'tsx': 'never',
+        }],
     },
+    settings: {
+        'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx']
+        },
+        'import/resolver': {
+            'node': {
+                'extensions': ['.js', '.jsx', '.ts', '.tsx']
+            }
+        }
+    }
 };
--- test/App.spec.tsx
+++ test/App.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
-import App from '../src/App.tsx';
+import App from '../src/App';
 
 describe('App', () => {
     it('should render correctly', () => {
